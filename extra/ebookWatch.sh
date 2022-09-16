ffd5fa067e526dd783e12c6f4d07ac6bc5f4c196
Christian Vuerings <cvuerings@pinterest.com>
2022-09-16 13:36:30 -0700

    Changes

--- extra/ebookWatch.sh
+++ extra/ebookWatch.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
 echo "Ebook watch started"
 
-/usr/local/bin/fswatch -0 -e ".*" -i ".epub" --event=Created ~/Dropbox\ \(Personal\)/Documents/Books/ --event Created | xargs -0 -n 1 -I {} ~/code/dotfiles/extra/ebookConvert.sh "{}"
+/opt/homebrew/bin/fswatch -0 -e ".*" -i ".epub" --event=Created ~/Dropbox\ \(Personal\)/Documents/Books/ --event Created | xargs -0 -n 1 -I {} ~/code/dotfiles/extra/ebookConvert.sh "{}"
