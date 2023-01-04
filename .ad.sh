c96efc1996f9a6e5b3a9d7c2d53bee393bd53e68
Christian Vuerings <cvuerings@pinterest.com>
2023-01-03 16:54:49 -0800

    Remove Pinterest specific logic

--- .ad.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/expect -f
-set timeout -1
-spawn arc diff --nolint --nounit
-match_max 100000
-expect "Provide explanation for skipping lint"
-send -- "remote\r"
-expect "Provide explanation for skipping unit"
-send -- "remote\r"
-expect eof
