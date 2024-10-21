07e00a9d52f284f864c72d8fcb02607056c81e23
Christian Vuerings <vueringschristian@gmail.com>
2024-10-21 06:03:37 -0700

    Zsh updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
 # git pull origin master;
 
@@ -8,12 +8,21 @@ function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
+		--exclude ".macos" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+		-avh --no-perms . ~
+	source ~/.bash_profile
+
+	if [ -n "$ZSH_VERSION" ]; then
+		source ~/.zshrc
+	elif [ -n "$BASH_VERSION" ]; then
+		source ~/.bash_profile
+	else
+		echo 'unknown shell'
+	fi
 }
 
-doIt;
-unset doIt;
+doIt
+unset doIt
